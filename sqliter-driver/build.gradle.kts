b4b4a553d737572b3c7643eecdbe4b980c6a6e36
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-02-26 17:23:56 +0100

    Prevent cross compilation on ci

--- sqliter-driver/build.gradle.kts
+++ sqliter-driver/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.konan.target.HostManager
+
 plugins {
     kotlin("multiplatform") version "1.4.20"
 }
@@ -8,59 +12,59 @@ val VERSION_NAME: String by project
 group = GROUP
 version = VERSION_NAME
 
-val ideaActive = System.getProperty("idea.active") == "true"
+val isIdeaActive = System.getProperty("idea.active") == "true"
 
-fun configInterop(target: org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget) {
+fun configInterop(target: KotlinNativeTarget) {
     val main by target.compilations.getting
     val sqlite3 by main.cinterops.creating {
         includeDirs("$projectDir/src/include")
     }
+
+    target.compilations.forEach { kotlinNativeCompilation ->
+        kotlinNativeCompilation.kotlinOptions.freeCompilerArgs += when {
+            HostManager.hostIsLinux -> listOf(
+                "-linker-options",
+                "-lsqlite3 -L/usr/lib/x86_64-linux-gnu" //just /usr/lib for arch
+            )
+            HostManager.hostIsMingw -> listOf("-linker-options", "-Lc:\\msys64\\mingw64\\lib")
+            else -> listOf("-linker-options", "-lsqlite3")
+        }
+    }
 }
 
-val onWindows = org.jetbrains.kotlin.konan.target.HostManager.hostIsMingw
-
-kotlin {
-    val knTargets = if (ideaActive) {
+fun KotlinMultiplatformExtension.registerTargets(): List<KotlinNativeTarget> {
+    return if (isIdeaActive) {
         listOf(
             macosX64("nativeCommon"),
             mingwX64("mingw"),
             linuxX64()
         )
     } else {
-        listOf(
-            macosX64(),
-            iosX64(),
-            iosArm64(),
-            iosArm32(),
-            watchosArm32(),
-            watchosArm64(),
-            watchosX86(),
-            tvosArm64(),
-            tvosX64(),
-            mingwX64("mingw") {
-                compilations.forEach {
-                    it.kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-Lc:\\msys64\\mingw64\\lib")
-                }
-            },
-            linuxX64 {
-                compilations.forEach {
-                    it.kotlinOptions.freeCompilerArgs += listOf(
-                        "-linker-options",
-                        "-lsqlite3 -L/usr/lib/x86_64-linux-gnu" //just /usr/lib for arch
-                    )
-                }
-            }
-        )
+        //prevent some cross compilation to speed up builds on CI
+        when {
+            HostManager.hostIsLinux -> listOf(linuxX64())
+            HostManager.hostIsMingw -> listOf(mingwX64("mingw"))
+            else -> listOf(
+                macosX64(),
+                iosX64(),
+                iosArm64(),
+                iosArm32(),
+                watchosArm32(),
+                watchosArm64(),
+                watchosX86(),
+                tvosArm64(),
+                tvosX64()
+            )
+        }
     }
+}
+
+kotlin {
+    val knTargets = registerTargets()
 
     knTargets
         .forEach { target ->
             configInterop(target)
-            val test by target.compilations.getting
-
-            if (!target.name.startsWith("mingw") && !target.name.startsWith("linux")) {
-                test.kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-lsqlite3")
-            }
         }
 
     sourceSets {
@@ -86,7 +90,7 @@ kotlin {
             dependsOn(nativeCommonMain)
         }
 
-        if (!ideaActive) {
+        if (!isIdeaActive) {
             val mingwMain = sourceSets.maybeCreate("mingwMain").apply {
                 dependsOn(nativeCommonMain)
             }
